23ebbb8afb535470702ac4b5ae40fa1e51ffd9e9
miniminiming <miniminiming@users.noreply.github.com>
2017-11-08 13:30:59 +0800

    添加注释，修改电影的图片资源

--- project_2/entertainment_center.py
+++ project_2/entertainment_center.py
@@ -1,11 +1,19 @@
 import media
 import fresh_tomatoes
 
-movie1 = media.Movie("Angry Bird","https://en.wikipedia.org/wiki/The_Angry_Birds_Movie#/media/File:The_Angry_Birds_Movie_poster.png","https://www.youtube.com/watch?v=1U2DKKqxHgE")
-movie2 = media.Movie("Storks","https://upload.wikimedia.org/wikipedia/en/1/13/Storks_%28film%29_poster_2.jpg","https://www.youtube.com/watch?v=ZVzL94jZNdU")
-movie3 = media.Movie("Rio","https://upload.wikimedia.org/wikipedia/en/b/bb/Rio2011Poster.jpg","https://www.youtube.com/watch?v=Bf6zeRwk5LE")
-movie4 = media.Movie("minions","https://en.wikipedia.org/wiki/Minions_(film)#/media/File:Minions_poster.jpg","https://www.youtube.com/watch?v=eisKxhjBnZ0")
+movie1 = media.Movie("Angry Bird",
+                     "https://ss0.bdstatic.com/94oJfD_bAAcT8t7mm9GUKT-xh_/timg?image&quality=100&size=b4000_4000&sec=1510118511&di=8e0fc4a16dcf0edf4f203e15317baa30&src=http://www.91danji.com/upload/201142/20114214513580.jpg",
+                     "https://www.youtube.com/watch?v=1U2DKKqxHgE")
+movie2 = media.Movie("Storks",
+                     "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1510128641760&di=fdecf86bd94913fa809e567dc897217f&imgtype=0&src=http%3A%2F%2Fimg1.gtimg.com%2Fent%2Fpics%2Fhv1%2F189%2F225%2F2107%2F137065239.jpg",
+                     "https://www.youtube.com/watch?v=ZVzL94jZNdU")
+movie3 = media.Movie("Rio",
+                     "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1510128743968&di=59170aef952975ab9ab86021067bbb8e&imgtype=0&src=http%3A%2F%2Fimg.25pp.com%2Fuploadfile%2Fapp%2Ficon%2F20160731%2F1469966124489411.jpg",
+                     "https://www.youtube.com/watch?v=Bf6zeRwk5LE")
+movie4 = media.Movie("minions",
+                     "https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=3568029076,2951212495&fm=27&gp=0.jpg",
+                     "https://www.youtube.com/watch?v=eisKxhjBnZ0")
 
-movies =[movie1,movie2,movie3,movie4]
+movies = [movie1, movie2, movie3, movie4]
 
 fresh_tomatoes.open_movies_page(movies)
--- project_2/media.py
+++ project_2/media.py
@@ -1,13 +1,16 @@
+'''
+ movies info class
+'''
+
+
 class Movie():
-
-	def __init__(self , title , poster_image_url , trailer_youtube_url):
-		self.title = title
-		self.poster_image_url = poster_image_url
-		self.trailer_youtube_url = trailer_youtube_url
-
-
-#<object type="application/x-shockwave-flash" id="StrobeMediaPlayback" name="StrobeMediaPlayback" align="middle" data="https://static.huomao.com/swf/huomaoplayer.swf?ver=2017090003000" width="100%" height="100%"><param name="quality" value="high"><param name="bgcolor" value="#222222"><param name="allowscriptaccess" value="always"><param name="allowFullScreenInteractive" value="true"><param name="allowfullscreen" value="true"><param name="wmode" value="Opaque"><param name="flashvars" value="initSize=big&amp;VideoIDS=DdvfHYeYleGdPEwYLVQ&amp;streamtype=live&amp;autostart=true&amp;url=https://www.huomao.com&amp;s=swf&amp;g=get_video_data&amp;uid=15719697&amp;nickname=qq_KL796FPnOSRA&amp;anchor_uid=1871673&amp;flash_chartleft=&amp;flash_chartright=&amp;flash_bg_img=https://livepic.huomao.com/11836/i_10.jpg?1507820400&amp;live_id=11836&amp;room_number=11836&amp;hasDanmuSetting=1&amp;hasRefresh=1&amp;hidePauseTip=1&amp;hideRightMenu=1&amp;hasDanmu=1&amp;hasFitPage=1&amp;hasLowBitrate=1&amp;needChat=1&amp;hasFeedBack=1&amp;hasRotabtn=0&amp;goimURL=ws://gate.huomao.com:8090/sub"></object>
-
-#https://livepic.huomao.com/11836/i_10.jpg?1507819800
-#https://static.huomao.com/upload/web/images/channel/2017/35/20170831140648jk32ZtQl.jpg
-#https://livepic.huomao.com/13298/i_10.jpg?1507819800
+    def __init__(self, title, poster_image_url, trailer_youtube_url):
+        '''
+         init movie info
+         :param title  movie's tittle
+         :param poster_image_url movie's preview photo url
+         :param trailer_youtube_url movie's preview video url
+        '''
+        self.title = title
+        self.poster_image_url = poster_image_url
+        self.trailer_youtube_url = trailer_youtube_url
